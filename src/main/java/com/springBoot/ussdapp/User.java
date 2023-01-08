97b1ef792a5bbf55e2545a009e846af01a695ea1
NicksyRop <nicksonkipkorir25@gmail.com>
2023-01-08 13:45:08 +0300

    added column properties

--- src/main/java/com/springBoot/ussdapp/User.java
+++ src/main/java/com/springBoot/ussdapp/User.java
@@ -1,19 +1,41 @@
 package com.springBoot.ussdapp;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
+
+    @Column(
+            name = "id",
+            updatable = false
+    )
     private Long id ;
+
+    @Column(
+            name = "first_name",
+            nullable = false,
+            columnDefinition = "text"
+    )
     private String firstName;
+    @Column(
+            name = "last_name",
+            nullable = false,
+            columnDefinition = "text"
+    )
     private String secondName;
+    @Column(
+            name = "email",
+            nullable = false,
+            columnDefinition = "text",
+            unique = true
+    )
     private String email;
+    @Column(
+            name = "id_number"
+    )
     private Integer idNumber;
 
     public User(Long id, String firstName, String secondName, String email, Integer idNumber) {
